e03ca463262ffca42cfa414e97e914ffe12822c6
uniqcle <uniqcle@yandex.ru>
2021-04-13 11:13:30 +0700

    auto write text

---
+++ script.js
@@ -0,0 +1,13 @@
+const text = 'I learning JavaScript. And I love it!!!'; 
+
+let index = 0; 
+
+function writeText(){
+    document.body.innerText = text.slice(0, index);
+    index++;  
+    if( index > text.length ){
+        index = 0; 
+    }
+}
+
+setInterval(writeText, 100); 
